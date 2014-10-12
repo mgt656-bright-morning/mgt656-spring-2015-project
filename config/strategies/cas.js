6652d4dceeab964e7b50d9314dd0b037009bddb3
Kyle L. Jensen <kljensen@gmail.com>
2014-10-12 12:43:16 -0400

    Fix Yale CAS settings

--- config/strategies/cas.js
+++ config/strategies/cas.js
@@ -8,12 +8,13 @@ module.exports = function(){
   passport.use(new (CasStrategy)({
     version: 'CAS3.0',
     ssoBaseURL: 'https://secure.its.yale.edu/cas',
-    serverBaseURL: 'http://localhost:3000/cas',
+    serverBaseURL: 'http://localhost:3000',
+    serviceURL: 'http://localhost:3000/auth/cas',
     validateURL: '/serviceValidate'
   }, function(profile, done) {
-    var login = profile.user;
+    var netid = profile.user;
     console.log(profile);
-    User.findOne({login: login}, function (err, user) {
+    User.findOne({netid: netid}, function (err, user) {
       if (err) {
         return done(err);
       }
