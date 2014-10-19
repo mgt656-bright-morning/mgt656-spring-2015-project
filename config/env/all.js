23205e0256c6ea7e97dc52a9818ffa82c9116565
Kyle L. Jensen <kljensen@gmail.com>
2014-10-19 14:49:12 -0400

    Render index on server side and include basic course info

--- config/env/all.js
+++ config/env/all.js
@@ -14,6 +14,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/fontawesome/css/font-awesome.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
