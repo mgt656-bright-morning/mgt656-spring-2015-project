04f17bc85b789a3e1d055d4796c2976759399d2b
Kyle L. Jensen <kljensen@gmail.com>
2014-10-21 21:39:04 -0400

    Add YAML front matter parsing [WIP]

--- app/models/class.server.model.js
+++ app/models/class.server.model.js
@@ -1,6 +1,14 @@
 'use strict';
+module.exports = exports = {classes: []};
 var glob = require('glob');
+var yamlFront = require('yaml-front-matter');
 
-module.exports = glob('app/models/classes_data/*.yaml', function(err, files){
+
+glob('app/models/classes_data/*.yaml', function(err, files){
   console.log(files);
+  for (var i = files.length - 1; i >= 0; i--) {
+    exports.classes.push(yamlFront.loadFront(files[i], 'content'));
+  }
+  console.log(exports.classes);
 });
+
