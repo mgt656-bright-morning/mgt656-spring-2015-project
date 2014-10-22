1d26511785ff66eb12765b52bdc5e509ac1ec646
Kyle L. Jensen <kljensen@gmail.com>
2014-10-22 10:32:43 -0400

    Move class schedule to index page.

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -6,9 +6,9 @@ var meetings = require('../models/meeting.server.model.js');
  * Module dependencies.
  */
 exports.index = function(req, res) {
-	res.render('index');
+	res.render('index', {meetings: meetings.meetings});
 };
 
-exports.schedule = function(req, res) {
-  res.render('schedule', {meetings: meetings.meetings});
-};
+// exports.schedule = function(req, res) {
+//   res.render('schedule', {meetings: meetings.meetings});
+// };
--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -2,8 +2,8 @@
 var users = require('../../app/controllers/users');
 
 module.exports = function(app) {
-	// Root routing
-	var core = require('../../app/controllers/core');
+  // Root routing
+  var core = require('../../app/controllers/core');
   app.route('/').get(users.requiresLogin, core.index);
-	app.route('/schedule').get(users.requiresLogin, core.schedule);
+  // app.route('/schedule').get(users.requiresLogin, core.schedule);
 };
