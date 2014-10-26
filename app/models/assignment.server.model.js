406bcf0a7a0df8f573b4e2ea736bbe9ad3512ea5
Kyle L. Jensen <kljensen@gmail.com>
2014-10-26 13:19:05 -0400

    Move assignment data/config to the models directory

--- app/models/assignment.server.model.js
+++ app/models/assignment.server.model.js
@@ -4,22 +4,25 @@ var glob = require('glob');
 var yamlFront = require('yaml-front-matter');
 var _ = require('lodash');
 var path = require('path');
+var fs = require('fs');
 
 // Takes in a assignments and returns 
 function makeAssignment (filePath) {
   var assignment = yamlFront.loadFront(filePath, 'content');
-  assignment.slug = path.basename(filePath, '.yaml');
+  assignment.slug = path.basename(path.dirname(filePath));
   return assignment;
 }
 
 
-// Find all the assignments from their YAML files
+// Find all the assignments and load their config
+// files as well as Schemas.
 //
-glob('data/assignments/*.yaml', function(err, files){
-  for (var i = files.length - 1; i >= 0; i--) {
-    exports.all.push(makeAssignment(files[i]));
-  }
-});
+var subDir = '/assignments/';
+var assignmentNames = fs.readdirSync(__dirname + subDir);
+for (var i = assignmentNames.length - 1; i >= 0; i--) {
+  var configPath = __dirname + subDir + assignmentNames[i] + '/config.yaml';
+  exports.all.push(makeAssignment(configPath));
+}
 
 exports.findBySlug = function (slug) {
   return _.find(exports.all, {slug: slug});
