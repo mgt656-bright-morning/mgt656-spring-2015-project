9a19457fcd203d35569f6609eb1f5dc4ecc8216b
Kyle L. Jensen <kljensen@gmail.com>
2014-10-26 22:08:17 -0400

    Add function for creating code field on homework assignments

--- app/models/assignments/defaults/defaults.js
+++ app/models/assignments/defaults/defaults.js
@@ -1,5 +1,7 @@
+'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var _ = require('lodash');
 
 module.exports = exports = {};
 
@@ -7,4 +9,15 @@ exports.schemaObject = {
   dateSubmitted: { type: Date, default: Date.now},
   points: {type: Number, min: 0, max: 100},
   wasSubmitted: {type: Boolean, default: false}
+};
+
+exports.getCodeForm = function (overloads) {
+  var defaults = {
+      textarea: true, 
+      help: 'Paste in the code you wrote here.', 
+      name: 'code', 
+      codeLanguage: 'markup', 
+      rows: 20
+  };
+  return _.assign(defaults, overloads);
 };
--- app/models/assignments/hw1a/model.js
+++ app/models/assignments/hw1a/model.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var defaultSchema = require('../defaults/defaults.js').schemaObject;
+var defaults = require('../defaults/defaults.js');
+var defaultSchema = defaults.schemaObject;
 var _ = require('lodash');
 
 module.exports = exports = {};
@@ -9,14 +10,10 @@ exports.schemaObject = _.assign(defaultSchema, {
   code: {
     type: String,
     match: /^.{1,5000}/,
-    form: {
-      title: 'Dash Project 1 code', 
-      textarea: true, 
-      help: 'Paste in the code you wrote for Project 1 here.', 
-      name: 'code', 
-      codeLanguage: 'markup', 
-      rows: 20
-    }
+    form: defaults.getCodeForm({
+      title: 'Dash Project 2 code', 
+      help: 'Paste in the code you wrote for Project 2 here.', 
+    })
   },
 });
 
