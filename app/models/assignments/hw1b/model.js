461d421ef99562d82f5cddfcb28d6030f480a821
Kyle L. Jensen <kljensen@gmail.com>
2014-10-26 20:51:18 -0400

    Fix h11b model

--- app/models/assignments/hw1b/model.js
+++ app/models/assignments/hw1b/model.js
@@ -6,5 +6,18 @@ var _ = require('lodash');
 module.exports = exports = {};
 
 exports.schemaObject = _.assign(defaultSchema, {
-  code: {type: String, match: /^.{1,5000}/, inForm: true},
+  code: {
+    type: String,
+    match: /^.{1,5000}/,
+    form: {
+      title: 'Dash Project 2 code', 
+      textarea: true, 
+      help: 'Paste in the code you wrote for Project 2 here.', 
+      name: 'code', 
+      codeLanguage: 'markup', 
+      rows: 20
+    }
+  },
 });
+
+exports.formFields = ['code'];
