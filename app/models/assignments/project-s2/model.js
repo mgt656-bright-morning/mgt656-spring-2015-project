3896797c5ea3421899a00c5df1fcff567c172fe0
Kyle L. Jensen <kljensen@gmail.com>
2014-11-10 03:50:50 -0500

    Add in the project sprints

---
+++ app/models/assignments/project-s2/model.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var defaults = require('../defaults/defaults.js');
+var defaultSchema = defaults.schemaObject;
+var _ = require('lodash');
+
+module.exports = exports = {};
+
+exports.schemaObject = _.assign(defaultSchema, {
+  code: {
+    type: String,
+    match: /^.{1,5000}/,
+    form: defaults.getCodeForm({
+      title: 'Dash Project 1 code', 
+      help: 'Paste in the code you wrote for Project 1 here.', 
+    })
+  },
+});
+
+exports.formFields = ['code'];
